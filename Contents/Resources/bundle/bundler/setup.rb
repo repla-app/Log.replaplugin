d4ae7192779130231e9fcada2ac5e053a683e6fe
Roben Kleene <robenk@gmail.com>
2015-11-26 16:50:17 -0500

    Bump ruby gems

--- Contents/Resources/bundle/bundler/setup.rb
+++ Contents/Resources/bundle/bundler/setup.rb
@@ -3,4 +3,4 @@ require 'rbconfig'
 ruby_engine = defined?(RUBY_ENGINE) ? RUBY_ENGINE : 'ruby'
 ruby_version = RbConfig::CONFIG["ruby_version"]
 path = File.expand_path('..', __FILE__)
-$:.unshift "#{path}/../#{ruby_engine}/#{ruby_version}/gems/webconsole-0.1.18/lib"
+$:.unshift "#{path}/../#{ruby_engine}/#{ruby_version}/gems/webconsole-0.1.19/lib"
