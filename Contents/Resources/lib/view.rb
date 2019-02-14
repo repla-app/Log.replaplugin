e6e3fd4b1fd17a1dcad27a10ef09d3715d686a80
Roben Kleene <contact@robenkleene.com>
2019-02-14 13:59:43 -0500

    Use load file

--- Contents/Resources/lib/view.rb
+++ Contents/Resources/lib/view.rb
@@ -2,13 +2,13 @@ require_relative '../bundle/bundler/setup'
 
 module WebConsole::Log
   class View < WebConsole::View
-    BASE_DIRECTORY = File.join(File.dirname(__FILE__), "..")
-    VIEWS_DIRECTORY = File.join(BASE_DIRECTORY, "view")
-    VIEW_TEMPLATE = File.join(VIEWS_DIRECTORY, 'view.html.erb')
+
+    ROOT_ACCESS_DIRECTORY = File.join(File.dirname(__FILE__), '../html')
+    VIEW_TEMPLATE = File.join(ROOT_ACCESS_DIRECTORY, 'index.html')
 
     def initialize
       super
-      self.base_url_path = File.expand_path(BASE_DIRECTORY)
+      self.root_access_directory_path = File.expand_path(ROOT_ACCESS_DIRECTORY)
       load_erb_from_path(VIEW_TEMPLATE)
     end
 
