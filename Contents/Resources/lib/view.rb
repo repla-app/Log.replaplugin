6e1c280d0e5b871e65a104908e71e919311d6585
Roben Kleene <contact@robenkleene.com>
2019-03-25 12:01:18 -0400

    Support dark mode

--- Contents/Resources/lib/view.rb
+++ Contents/Resources/lib/view.rb
@@ -6,13 +6,18 @@ module Repla
     class View < Repla::View
       ROOT_ACCESS_DIRECTORY = File.join(__dir__, '../html')
       VIEW_TEMPLATE = File.join(ROOT_ACCESS_DIRECTORY, 'index.html')
+      DARK_VIEW_TEMPLATE = File.join(ROOT_ACCESS_DIRECTORY, 'dark.html')
 
       def initialize
         super
         self.root_access_directory_path = File.expand_path(
           ROOT_ACCESS_DIRECTORY
         )
-        load_file(VIEW_TEMPLATE)
+        if dark_mode
+          load_file(DARK_VIEW_TEMPLATE)
+        else
+          load_file(VIEW_TEMPLATE)
+        end
       end
 
       def log_error(message)
