479449c4d3652770b1f31eecf700bb675091e014
Roben Kleene <contact@robenkleene.com>
2019-03-25 17:21:42 -0400

    Support dark mode (#10)
    * Bump gem
    
    * Add dark assets
    
    * Support dark mode
    
    * Make color white
    
    * Support dark mode
    
    * Add transparent background

--- Contents/Resources/lib/view.rb
+++ Contents/Resources/lib/view.rb
@@ -6,13 +6,15 @@ module Repla
     class View < Repla::View
       ROOT_ACCESS_DIRECTORY = File.join(__dir__, '../html')
       VIEW_TEMPLATE = File.join(ROOT_ACCESS_DIRECTORY, 'index.html')
+      DARK_VIEW_TEMPLATE = File.join(ROOT_ACCESS_DIRECTORY, 'dark.html')
 
       def initialize
         super
         self.root_access_directory_path = File.expand_path(
           ROOT_ACCESS_DIRECTORY
         )
-        load_file(VIEW_TEMPLATE)
+        template = dark_mode ? DARK_VIEW_TEMPLATE : VIEW_TEMPLATE
+        load_file(template)
       end
 
       def log_error(message)
